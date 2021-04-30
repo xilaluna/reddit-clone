a178f5e3217f61fbe4600e259d1dfc70768935dd
Xila Luna <xilaluna2@gmail.com>
2021-04-29 22:00:21 -0700

    [add] comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const Post = require("../models/post")
 
 module.exports = (app) => {
+  // get all posts
   app.get("/", (req, res) => {
     Post.find({})
       .lean()
@@ -12,6 +13,24 @@ module.exports = (app) => {
       })
   })
 
+  app.get("/posts/new", (req, res) => {
+    console.log("Loading posts-new")
+    res.render("posts-new")
+  })
+
+  // CREATE
+  app.post("/posts/new", (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body)
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect("/")
+    })
+  })
+
+  // SUBREDDIT
   app.get("/n/:subreddit", function (req, res) {
     Post.find({ subreddit: req.params.subreddit })
       .lean()
@@ -23,20 +42,12 @@ module.exports = (app) => {
       })
   })
 
-  app.get("/posts/new", (req, res) => {
-    res.render("posts-new")
-  })
-
-  app.post("/posts/new", (req, res) => {
-    const post = new Post(req.body)
-    post.save((err, post) => {
-      return res.redirect("/")
-    })
-  })
-
+  // can search post by id
   app.get("/posts/:id", function (req, res) {
+    // LOOK UP THE POST
     Post.findById(req.params.id)
       .lean()
+      .populate("comments")
       .then((post) => {
         res.render("posts-show", { post })
       })
--- models/post.js
+++ models/post.js
@@ -6,6 +6,7 @@ const PostSchema = new Schema({
   url: { type: String, required: true },
   summary: { type: String, required: true },
   subreddit: { type: String, required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
 })
 
 module.exports = mongoose.model("Post", PostSchema)
